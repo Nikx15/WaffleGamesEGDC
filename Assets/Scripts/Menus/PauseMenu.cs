5689c5a129394d97814b6fe8b5347df2a237fd99
River! <123593150+blakestar67@users.noreply.github.com>
2024-03-01 20:45:35 -0500

    Pause Menu Working
    Also gives us time to add in the explosion animation.

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public float timerExplode = 3;
+    public bool TimerOn = false;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        TimerOn = false;
+        timerExplode = 3;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+        if (TimerOn)
+        {
+            if (timerExplode > 0)
+            {
+                timerExplode -= Time.deltaTime;
+                //updateTimer(timerExplode);
+                if (timerExplode < 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+        }
+
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void Detonate()
+    {
+        ResumeGame();
+        //player movement false
+        TimerOn = true;
+    }
+}
