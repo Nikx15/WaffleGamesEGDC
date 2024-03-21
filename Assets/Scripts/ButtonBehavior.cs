bf1d2285a0b83f3abf5689ae07ed1f86717bf724
Nikx <nikoagard131524@gmail.com>
2024-03-21 12:20:41 -0400

    Started the Button Doors [WIP]
    Have to compare Multiple Tags still

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet" "Player"))
+        {
+            door.SetActive(false);
+        }
+        else
+        {
+            door.SetActive(true);
+        }
+    }
+}
