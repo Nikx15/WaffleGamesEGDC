f4a84aa855d7211f31dfe2665b8cd4d6c59dc597
River! <123593150+blakestar67@users.noreply.github.com>
2024-04-05 20:04:46 -0400

    GUY DOESNT SPIN ANYMORE
    WEAPON IS FUCKMINSEJUFIEBGHSJ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Weapon : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireForce = 20f;
+
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    public Weapon weapon;
+    Vector2 mousePosition;
 
     public void Update()
     {
@@ -20,5 +26,19 @@ public class Weapon : MonoBehaviour
         //bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
 
         //change Fire() to update? should work completely fine because its triggered by a button
+
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+    }
+
+    void ProcessInputs()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
